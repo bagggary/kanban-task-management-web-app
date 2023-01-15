b3cf9387ca31536d3edf1617d9533ef1f29a4f04
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-01-15 23:01:33 +0300

    update folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import Navbar from './Components/Navbar'
 // import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      
-    </div> 
+    <div className='container'>
+      <Navbar />
+    </div>
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import darkLogo from '/src/assets/icons/logo-dark.svg'
+import lightLogo from '/src/assets/icons/logo-light.svg'
+import mobileLogo from '/src/assets/icons/logo-mobile.svg'
 
 export default function Navbar(){
-
+return (
+    <nav>
+        <div className="logo">
+        <picture>
+                    <source 
+                        srcset={darkLogo}
+                        media="(min-width: 600px)"
+                    />
+                    <img 
+                        src={lightLogo}
+                        alt="logo"
+                    />
+        </picture>
+        </div>
+    </nav>
+)
 }
