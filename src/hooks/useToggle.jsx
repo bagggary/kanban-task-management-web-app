d1883f03e7be74702eb05c85451b4f318a344feb
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-01-29 17:29:02 +0300

    added custom hook

---
+++ src/hooks/useToggle.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+export default function useToggle(defaultValue) {
+  const [value, setValue] = useState(defaultValue)
+
+  function toggleValue(value) {
+    setValue(currentValue =>
+      typeof value === "boolean" ? value : !currentValue
+    )
+  }
+
+  return [value, toggleValue , setValue]
+}
+
+
+
+
+
+// import React , { useState } from "react";
+
+// const useToggle = (initialState = false) => {
+//   const [visible, setVisibility] = useState(initialState);
+
+//   const toggle = useCallback(() => setVisibility((prev) => !prev), []);
+
+//   const setToggleStatus = (value) => setVisibility(Boolean(value));
+
+//   return [visible, toggle, setToggleStatus];
+// };
+// export default  useToggle;
