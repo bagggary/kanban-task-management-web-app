2c8b52421abed396133af77154216d47fb0c45cc
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-02-14 14:41:39 +0300

    create functions through props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React ,  { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React ,  { useRef, useState} from 'react'
 import Navbar from './UI/Navbar'
 import './main.css'
 import Sidenav from './UI/Sidenav'
 import useToggle  from "./hooks/useToggle";
 import boardData from './assets/data.json'
-import AddTask from './Forms/AddTask'
+import AddTask from './UI/Forms/AddTask'
+import AddBoard from './UI/Forms/AddBoard'
+import BoardDelete from './UI/BoardDelete'
+import TaskDelete from './UI/TaskDelete'
+import Overlay from './UI/Overlay'
 
 function App() {
 
@@ -13,6 +16,40 @@ function App() {
   const [data , setData ] = useState(boardData)
   const [selectedBoard , setSelectedBoard] = useState('')
   const [selectedId ,  setSelectedId] = useState(0)
+  const [formAppear , setFormAppear] = useState({
+    board : false ,
+    editBoard : false ,
+    overlay : false ,
+    boardOptions : false
+
+  })
+  // const sharedRefs = useRef(null)
+  // useEffect(() => {
+  //   function outsideClick(event) {
+  //     if (sharedRefs.current && !sharedRefs.current.contains(event.target)) {
+  //       setFormAppear(prev => {
+  //           return {
+  //               ...prev,
+  //               board: false,
+  //               overlay: false
+  //           }
+  //       });
+  //     }
+  //   }
+  //   document.addEventListener('mousedown', outsideClick);
+  //   return () => {
+  //     document.removeEventListener('mousedown', outsideClick);
+  //   };
+  // }, [sharedRefs]);
+
+  // const updateBoard = (obj) => {
+  //   setData(prev => {
+  //     return [
+  //       ...prev.boards ,
+  //       obj 
+  //     ]
+  //   }, obj)
+  // }
 
     function handleActive(id){
         setSelectedId(id)
@@ -20,12 +57,15 @@ function App() {
 
   return (
     <>
-      {/* <Navbar 
+      <Navbar 
       openNav = {side}
       toggleNav = {toggle} 
       data = {data}
       handleBoard = {(e) => handleActive(e.target.id)}
       selectBoard = {selectedId}   
+      formAppear = {formAppear}
+      setFormAppear = {setFormAppear}
+      setData = {setData}
            />
       <div className='content'>
       <Sidenav 
@@ -33,13 +73,16 @@ function App() {
       side = {side}
       toggle = {toggle}
       data = {data}
-      selectBoard = {selectedId}  
+      selectBoard = {selectedId}
+      formAppear = {formAppear}
+      setFormAppear = {setFormAppear}
+      setData = {setData}
        />
       <div className= {`main-content transition container ${side ? '' : 'content-screen'}`}  >
         <div>task management</div>
       </div>
-      </div> */}
-      <AddTask />
+      </div>
+      {formAppear.overlay && <Overlay />}
     </>
   )
 }
