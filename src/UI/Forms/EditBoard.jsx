c7bef0067a5e44323d9449a42bb78c9e6007b456
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-02-14 12:07:23 +0300

    edit board ui form

--- src/UI/Forms/EditBoard.jsx
+++ src/UI/Forms/EditBoard.jsx
@@ -1,41 +1,87 @@
-import React, { useState , useEffect } from 'react'
+import React, { useState , useEffect , useRef } from 'react'
 
-export default function EditBoard ({formAppear , data , selectBoard , setData}) {
+export default function EditBoard ({formAppear , data , selectBoard , setData , setFormAppear }) {
     const [dataState , setDataState] = useState(null)
+    const [formErrors, setFormErrors] = useState({});
+    const boardRef = useRef(null)
+    useEffect(() => {
+        function outsideClick(event) {
+          if (boardRef.current && !boardRef.current.contains(event.target)) {
+            setFormAppear(prev => {
+                return {
+                    ...prev,
+                    editBoard: false,
+                    overlay: false
+                }
+            });
+          }
+        }
+        document.addEventListener('mousedown', outsideClick);
+        return () => {
+          document.removeEventListener('mousedown', outsideClick);
+        };
+      }, [boardRef]); 
+
     useEffect(()=> {
         setDataState(data[selectBoard])
     } , [data])
 
-    function addCol(){
-        const newBoardCol = {name: "", tasks: []}
-        const newArray = data.map((item, i) => {
-            if (selectBoard === i) {
-              return { ...item, columns: [...item.columns , newBoardCol] };
-            } else {
-              return item;
+    function removeSub(id){
+        if(dataState.columns.length <= 2){
+            return 
+        }
+        const newColumnsData = [...dataState.columns];
+        newColumnsData.splice(id, 1);
+        setDataState(prev => {
+            return {
+                ...prev ,
+                columns : newColumnsData
             }
         });
-        setData(newArray);
-        validate()
-        // setDataState((prev) => {
-        //     return {
-        //         ...prev ,
-        //         columns : [...prev.columns , newBoardCol]
-        //     }
-        // });
-        // setData((prev) => {
-        //     return {
-        //         ...prev ,
-        //         columns : [...prev.columns , newBoardCol]
-        //     }
-        // })
-        // setBoardObj((prev) => {
-        //     return {
-        //         ...prev ,
-        //         columns : [...prev.columns , newBoardCol]
-        //     }
-        // })
     }
+
+    function addCol(){
+        const newBoardCol = {name: "", tasks: []}
+        setDataState(prev => {
+            return {
+                ...prev ,
+                columns : [...prev.columns , newBoardCol]
+            }
+        });
+    }
+    const handleSumbit = (e)=> {
+        e.preventDefault();
+        const errors = validate();
+        if (Object.keys(errors).length === 0) {
+            setData((prev) => {
+                const newDataState = [...prev]
+                newDataState[selectBoard] = dataState
+                return newDataState
+            });
+            setFormAppear(prev => {
+                return {
+                    ...prev,
+                    overlay: false ,
+                    editBoard : false
+                }
+            });
+        } else {
+            setFormErrors(errors);
+        }
+    }
+
+    const handleAddBoard = (e, index ) => {
+        const columns = [...dataState.columns];
+        columns[index].name = e.target.value;
+        setDataState( prev => {
+            return (
+                {
+                ...prev,
+                columns }
+            )
+            } )  
+    }
+         
     const validate = () => {
         const errors = {}
         if(!dataState.name) {
@@ -48,37 +94,42 @@ export default function EditBoard ({formAppear , data , selectBoard , setData}) 
                 errors[`err-${index}`] = true
             }
         })
-        console.log(errors)
+        return errors
+    }
+    function titleHandle(e) {
+        setDataState((prev) => {
+            return {
+                ...prev , 
+                name : e.target.value
+            }
+        })
     }
 
   return (
-    <div className='add-new edit transition'  >
+    <div className='add-new edit transition' ref={boardRef} >
          <h1>Edit Board</h1>
         <form>
-            <div className={`f-tit ${true && `error`}`}>
+            <div className={`f-tit ${formErrors.name && `error`}`}>
                 <label htmlFor="title">BoardName</label>
-                <input type="text" name='name' id='title' placeholder='e.g. Take coffee break'   value = {data[selectBoard].name}/> 
-                {/* <p>{formErrors.name && formErrors.name}</p> */}
-                {/* onChange={titleHandle} */}
+                <input type="text" name='name' id='title' placeholder='e.g. Take coffee break'   value = {dataState && dataState.name} onChange = { titleHandle}/> 
+                <p>{formErrors.name && formErrors.name}</p>
                  </div>
             <div className='f-sub'> 
                 <label >BoardColumns</label>
                 <div className='sub-styles'>
-                {data[selectBoard].columns && data[selectBoard].columns.map((col , index) => {
+                {dataState && dataState.columns.map((col , index) => {
                     return (
-                        // className={`sub-${index} ${formErrors[`err-${index}`] && `error`}`}
-                        // onChange={(e) => handleAddBoard(e, index)}
-                        // onClick={revomveCol }
-                        <div key={index} className={`sub-1`} id={index}>
-                        <input type="text" id={index}  placeholder={`e.g. Col-${index + 1} `}  value = {col.name} />
-                        <svg  width="15" height="15" xmlns="http://www.w3.org/2000/svg"><g  fill-rule="evenodd"><path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z"/><path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z"/></g></svg>
-                        {/* <p> {formErrors[`col-${index}`] && formErrors[`col-${index}`]}</p> */} </div>
+                        <div key={index}  className={`sub-${index} ${formErrors[`err-${index}`] && `error`}`}id={index}>
+                        <input type="text" id={index}  placeholder={`e.g. Col-${index + 1} `}  value = {col.name}  onChange = { (e) => handleAddBoard(e, index)} />
+                        <svg onClick={(e) => removeSub(index) } width="15" height="15" xmlns="http://www.w3.org/2000/svg"><g  fill-rule="evenodd"><path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z"/><path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z"/></g></svg>
+                        <p> {formErrors[`col-${index}`] && formErrors[`col-${index}`]}</p>
+                         </div>
                     )
                 })}
                 </div>
                 <button type='button' onClick={addCol}> + Add New Column</button>
             </div>
-            <button type='button'>Create New Board</button>
+            <button type='button' onClick={handleSumbit}>Create New Board</button>
         </form> 
     </div>
   )
