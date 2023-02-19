1266c9d7efa4225b45709dada7a9b08e3589f236
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-02-19 18:01:56 +0300

    adding task component

--- src/UI/Forms/AddTask.jsx
+++ src/UI/Forms/AddTask.jsx
@@ -1,56 +1,195 @@
 import React, { useState , useEffect } from 'react'
 import useToggle from '../../hooks/useToggle'
 
-export default function AddTask({ selectBoard , formAppear , data ,setData ,setFormAppear }) {
+export default function AddTask({ selectBoard , formAppear , data , setData , setFormAppear }) {
 
     const [show , toggleShow ] = useToggle(false)
-    const [column , setColumn] = useState('Todo')
     const [dataTasks , setDataTasks] = useState([])
+    const [formErrors, setFormErrors] = useState({});
+    const [taskObj , setTaskObj] = useState( {
+        title : '' ,
+        description : '' ,
+        status : '' ,
+        subtasks : [
+            {title : "" , isCompletd : false} ,
+            {title : "" , isCompletd : false}
+        ] ,
+    })
+    const [sub , setSub] = useState([
+        {title : '' , placeholder : 'e.g. Make coffee'},
+        {title : '' , placeholder : 'e.g. Drink coffee & smile'}
+    ])
     useEffect(()=> {
         setDataTasks(data[selectBoard].columns)
-    } , [data])
-    console.log(dataTasks)
+        setTaskObj((prev) => {
+            return {
+                ...prev ,
+                status :  data[selectBoard].columns[0].name
+            }
+        })
+    } , [])
+    
+    const [status , setStatus] = useState('Todo')
+    function titleHandle(e) {
+        const {name , value} = e.target
+        setTaskObj((prev) => {
+            return {
+                ...prev , 
+                [name] : value
+            }
+        })
+    }
+
+//function to handle add the value of each subtasks onChange on each input field
+    const handleAddTask = (e, index ) => {
+        const subtasks = [...taskObj.subtasks];
+        subtasks[index].title = e.target.value;
+        setTaskObj( prev => {
+            return (
+                {
+                ...prev,
+                subtasks }
+            )
+            } )  
+         }
+// function to handle adding newSubTask
+function addSubTask(){
+    const newSub = {value : '' , placeholder : `e.g. New Subtask `}
+    const newSubTasks =  {title : "" , isCompletd : false}
+    setSub((prev) => {
+        return [
+            ...prev ,
+            newSub
+    ]
+    })
+    setTaskObj((prev) => {
+        return {
+            ...prev ,
+            subtasks : [...prev.subtasks , newSubTasks]
+        }
+    })
+}
+
+// adding validation to each input field to ensure that all input fileds are filled and then added to the main data set
+const validate = () => {
+    const errors = {};
+    if (!taskObj.title) {
+      errors.title = "Can\'t be empty";
+      errors.titleError = true
+    }
+    taskObj.subtasks.forEach((tsk , index) => {
+        if(!tsk.title) {
+            errors[`sub-${index}`] = 'Can\'t be empty'
+            errors[`err-${index}`] = true
+        }
+    })
+    return errors;
+};
+
+
+function handleStatus(e){
+    setStatus(e.target.textContent)
+    setTaskObj((prev) => {
+       return {
+           ...prev ,
+           status : status
+       }
+    })
+}
+
+
+const handleStatusChange = (event) => {
+    const selectedStatus = event.target.textContent;
+    setTaskObj((prevTaskObj) => ({
+      ...prevTaskObj,
+      status: selectedStatus
+    }));
+  };
+// handling submit function to set the main data to data object json
+
+
+
+
+const handleSumbit = (e)=> {
+    e.preventDefault();
+    const errors = validate();
+    if (Object.keys(errors).length === 0) {
+        setData((prev) => {
+            prev.map((col , index) => {
+                if(selectBoard === index ){
+                    for(let subTask of col.columns) {
+                        if(subTask.name === taskObj.status){
+                            return [
+                                ...prev ,
+                                {name : 'newObject'}
+                            ]
+                            // return [
+                            //     ...prev ,
+                            //     {...col , columns : [...col.columns , {...subTask , tasks : [...subTask.tasks , taskObj]}]}
+                            // ]
+                        }
+                    }
+                }else{
+                    return data
+                }
+            })
+        })
+        setFormAppear(prev => {
+            return {
+                ...prev,
+                task: false,
+                overlay: false
+            }
+        });
+    } else {
+        setFormErrors(errors);
+    }
+    
+}
   return (
     <div className="overlay">
          <div className='add-new task'>
         <h1>Add New Task</h1>
         <form>
-            <div className='f-tit'>
+            <div className={`f-tit ${formErrors.titleError && `error`}`}>
                 <label htmlFor="title">Title</label>
-                <input type="text" id='title' placeholder='e.g. Take coffee break' />
+                <input type="text" id='title' name = 'title'  placeholder='e.g. Take coffee break' onChange={(e) => titleHandle(e)}/>
+                <p>{formErrors.title && formErrors.title}</p>
             </div>
             <div className='f-des'>
                 <label htmlFor="des">Description</label>
-                 <textarea id='des' placeholder='e.g. It’s always good to take a break. This 15 minute break will 
-recharge the batteries a little.'></textarea>
+                 <textarea id='des' name='description' placeholder='e.g. It’s always good to take a break. This 15 minute break will 
+recharge the batteries a little.' onChange={(e) => titleHandle(e)}></textarea>
             </div>
             <div className='f-sub'>
                 <label htmlFor="sub">Subtask</label>
-                <div className="sub-styles">
-                <div className='sub - 1'>
-                    <input type="text" id='sub' placeholder='e.g. Make coffee'/>
-                    <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg"><g fill="#828FA3" fill-rule="evenodd"><path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z"/><path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z"/></g></svg>
+                <div className="sub-styles task">
+                    {sub && sub.map((subtsk , index) => {
+                        return (
+                            <div className={`sub-${index} ${formErrors[`err-${index}`] && `error`}`}>
+                         <input type="text" id='sub' placeholder={subtsk.placeholder} onChange = {(e) => handleAddTask(e , index)}/>
+                         <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg"><g  fill-rule="evenodd"><path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z"/><path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z"/></g></svg>
+                         <p> {formErrors[`sub-${index}`] && formErrors[`sub-${index}`]}</p>
+                     </div>
+                             )
+                    })}
                 </div>
-                <div className='sub - 2'>
-                    <input type="text" id='sub' placeholder='e.g. Drink coffee & smile'/>
-                    <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg"><g fill="#828FA3" fill-rule="evenodd"><path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z"/><path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z"/></g></svg>
-                </div>
-                </div>
-                <button type='button'> + Add New Subtask</button>
+                <button type='button' onClick={addSubTask}> + Add New Subtask</button>
             </div>
             {/* this has to change to cover the status of each board created to do so .  */}
             <div className='f-stat' onClick={toggleShow}>
-                <div className='stat-title'> Todo</div>
+                <div className='stat-title' >{status}</div>
                 <span><svg width="10" height="7" xmlns="http://www.w3.org/2000/svg"><path stroke="#635FC7" stroke-width="2" fill="none" d="m1 1 4 4 4-4"/></svg></span>
                 <div className={`stat-dropdown ${show ? 'show' : ''} transition`} >
-                    <ul>
-                        <li>Todo</li>
-                        <li>Doing</li>
-                        <li>Done</li>
+                    <ul>{dataTasks && dataTasks.map((tsk , index) => {
+                        return (
+                            <li key={index} id = {index} onClick = {handleStatusChange}  >{tsk.name}</li>
+                        )
+                    })}
                     </ul>
                 </div>
             </div>
-            <button type='button'>Create Task</button>
+            <button type='button' onClick={handleSumbit}>Create Task</button>
         </form>
     </div>
     </div>
