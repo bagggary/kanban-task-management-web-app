7d305e0a57c2338cc77f082e7efd6931f921710c
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-02-26 17:25:54 +0300

    change dir of taskDeails component to app.js dir

--- src/UI/TaksDetails.jsx
+++ src/UI/TaksDetails.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-export default function TaksDetails({ task, subtask }) {
+export default function TaksDetails({ task}) {
     return (
       <div className='detailed-info'>
         <div className='detailed-info-title'>
-          <h1>{task}</h1>
+          <h1>{task.title}</h1>
           <svg
             width='5'
             height='20'
             xmlns='http://www.w3.org/2000/svg'
           >
+
             <g fill='#828FA3' fill-rule='evenodd'>
               <circle cx='2.308' cy='2.308' r='2.308' />
               <circle cx='2.308' cy='10' r='2.308' />
@@ -18,7 +19,7 @@ export default function TaksDetails({ task, subtask }) {
         </div>
         <p>description paragraph</p>
         <div className='detailed-info-subtasks'>
-          {subtask.map((subtask) => (
+          {/* {subtask.map((subtask) => (
             <div className='detailed-info-subtasks-sub' key={subtask.id}>
               <label htmlFor={subtask.id}>{subtask.title}</label>
               <input
@@ -28,7 +29,7 @@ export default function TaksDetails({ task, subtask }) {
               defaultChecked={subtask.isCompleted}
             />
           </div>
-        ))}
+        ))} */}
       </div>
       <div className='detailed-info-status'>
         <h1> status</h1>
