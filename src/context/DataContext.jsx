28a300598614029a0d41b88714b226c1176922b9
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-01-16 19:47:14 +0300

    data context

---
+++ src/context/DataContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useReducer } from "react";
+import Data from "../assets/data.json";
+
+const DataContext = createContext(null);
+
+export const useDataContext = () => {
+  return useContext(DataContext);
+};
+
+const initialState = {
+  data: Data,
+};
+
+const dataReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "SET_DATA":
+      return {
+        ...state,
+        jsonData: payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const DataProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(dataReducer, initialState);
+
+  const setData = (data) => {
+    dispatch({ type: "SET_DATA", payload: data });
+  };
+
+  return (
+    <DataContext.Provider value={{ ...state, setData }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
