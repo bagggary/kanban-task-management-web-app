db794be0d69f90090fa6c792482cb21046a366c8
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-01-16 19:47:46 +0300

    save theme through local storage

--- src/UI/ThemeContext.jsx
+++ src/UI/ThemeContext.jsx
@@ -1,30 +1,40 @@
-import { createContext, useContext, useState, useLayoutEffect } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  useLayoutEffect,
+  useEffect,
+} from "react";
 const ThemeContext = createContext();
 const ThemeProvider = ({ children }) => {
-const [theme, setTheme] = useState('light');
-const toggleTheme = () =>
+  const themes = JSON.parse(localStorage.getItem("theme"));
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem("theme")) || "light"
+  );
+
+  const toggleTheme = () =>
     setTheme((theme) => (theme === "light" ? "dark" : "light"));
-    useLayoutEffect(() => {
-      if (theme === "dark") {
-  document.documentElement.setAttribute('data-theme', 'dark');
+  localStorage.setItem("theme", JSON.stringify(theme));
+  useLayoutEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.setAttribute("data-theme", "dark");
     } else {
-      document.documentElement.setAttribute('data-theme', 'light');
-      return ;
+      document.documentElement.setAttribute("data-theme", "light");
+      return;
     }
   }, [theme]);
-return (
+  return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
 };
 
-
 const useTheme = () => {
   const context = useContext(ThemeContext);
-if (context === undefined) {
+  if (context === undefined) {
     throw new Error("useTheme must be used within a ThemeProvider");
   }
-return context;
+  return context;
 };
 export { ThemeProvider, useTheme };
