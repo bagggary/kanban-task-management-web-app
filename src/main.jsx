6dce3fd081d8c9605f1a89ffe22a31d5cb8dd0f1
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-01-16 19:47:27 +0300

    update context provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { ThemeProvider } from './UI/ThemeContext'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "./UI/ThemeContext";
+import { DataProvider } from "./context/DataContext";
 // import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider>
-    <App />
+    <DataProvider>
+      <App />
+    </DataProvider>
   </ThemeProvider>
-)
+);
