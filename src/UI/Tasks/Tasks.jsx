0f39407f5900fec72385ea7d87c422a28cfc923c
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-01-31 00:07:44 +0300

    update taskDetails modals and functions

--- src/UI/Tasks/Tasks.jsx
+++ src/UI/Tasks/Tasks.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import TaksDetails from "./TasksDetails";
+import TaskDelete from "../Modal/TaskModal/TaskDelete";
+import EditTask from "../Modal/TaskModal/EditTask";
 
 export default function Tasks({ subtasks, task, columnId }) {
   const [isMounted, setIsMounted] = useState(false);
   const [details, setDetails] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [taskDelete, setTaskDelete] = useState(false);
   const completedTasks = () => {
     const completedSubtasks = subtasks.filter((subtask) => subtask.isCompleted);
     return completedSubtasks.length;
@@ -14,18 +18,34 @@ export default function Tasks({ subtasks, task, columnId }) {
   }, []);
 
   return (
-    <div
-      className={`task-card ${isMounted ? "scale-in" : ""}`}
-      onClick={() => setDetails(true)}
-    >
-      <h3>{task.title}</h3>
-      <p>{`${completedTasks()} of ${subtasks.length} subtasks`}</p>
+    <>
+      <div
+        className={`task-card ${isMounted ? "scale-in" : ""}`}
+        onClick={() => setDetails(true)}
+      >
+        <h3>{task.title}</h3>
+        <p>{`${completedTasks()} of ${subtasks.length} subtasks`}</p>
+      </div>
       <TaksDetails
         isOpen={details}
         onClose={() => setDetails(false)}
+        onOpenEdit={() => setEdit(true)}
+        onOpenDelete={() => setTaskDelete(true)}
         task={task}
         columnId={columnId}
       />
-    </div>
+      <EditTask
+        task={task}
+        isOpen={edit}
+        onClose={() => setEdit(false)}
+        columnId={columnId}
+      />
+      <TaskDelete
+        task={task}
+        isOpen={taskDelete}
+        onClose={() => setTaskDelete(false)}
+        columnId={columnId}
+      />
+    </>
   );
 }
