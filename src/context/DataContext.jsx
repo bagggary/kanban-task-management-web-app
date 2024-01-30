2bfd71179074a737e2163a005b512d5ffc562fe3
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-01-31 00:05:08 +0300

    integrate localStorage with context api to persist data on refresh

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import Data from "../assets/data.json";
 
 const DataContext = createContext(null);
@@ -6,10 +6,23 @@ const DataContext = createContext(null);
 export const useDataContext = () => {
   return useContext(DataContext);
 };
+const localData = localStorage.getItem("kanbanTasks");
+let initialState;
+try {
+  initialState = {
+    data: localData ? JSON.parse(localData) : Data,
+  };
+} catch (error) {
+  console.error("Error parsing localData:", error);
+  initialState = {
+    data: Data, // Provide a default value in case of parsing error
+  };
+}
 
-const initialState = {
-  data: Data,
-};
+// const localData = localStorage.getItem("kanbanTasks");
+// const initialState = {
+//   data: localData ? JSON.parse(localData) : Data,
+// };
 
 const dataReducer = (state, action) => {
   const { type, payload } = action;
@@ -29,6 +42,17 @@ export const DataProvider = ({ children }) => {
   const setData = (data) => {
     dispatch({ type: "SET_DATA", payload: data });
   };
+
+  useEffect(() => {
+    try {
+      const storedData = localStorage.getItem("kanbanTasks");
+      if (storedData && JSON.stringify(state.data) !== storedData) {
+        localStorage.setItem("kanbanTasks", JSON.stringify(state.data));
+      }
+    } catch (error) {
+      console.error("Error setting data in localStorage:", error);
+    }
+  }, [state.data]);
 
   return (
     <DataContext.Provider value={{ ...state, setData }}>
