e0b62760ab6e93b679f2bfa6b3199aed6e741a6a
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-01-31 00:07:14 +0300

    fixed newColumn function

--- src/UI/Home/home.jsx
+++ src/UI/Home/home.jsx
@@ -1,32 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSideContext } from "../../context/SideToggle";
 import { useDataContext } from "../../context/DataContext";
 import { useIdContext } from "../../context/IdContext";
 import Sidenav from "../Sidebar/Sidenav";
 import BoardStatus from "../Status/BoardStatus";
+import EditBoard from "../Modal/BoardModal/EditBoard";
 
 function Home() {
-  const { side, toggle } = useSideContext();
-  const { data, setData } = useDataContext();
-  const { id, setId } = useIdContext();
+  const { side } = useSideContext();
+  const { data } = useDataContext();
+  const { id } = useIdContext();
+  const [editColumn, setEditColumn] = useState(false);
   const board = data.filter((boardData) => boardData.id === id)[0];
   return (
-    <div className="content">
-      <Sidenav handleBoard={(e) => handleActive(e.target.id)} />
-      <div
-        className={`main-content transition container ${
-          side ? "" : "content-screen"
-        }`}
-      >
-        {board.columns.map((col, _) => {
-          return <BoardStatus key={col.id} boardId={col.id} column={col} />;
-        })}
-        {/* TODO : the new column div is set to handle editBoard */}
-        <div className="new-column">
-          <p>+ New Column</p>
+    <>
+      <div className="content">
+        <Sidenav handleBoard={(e) => handleActive(e.target.id)} />
+        <div
+          className={`main-content transition container ${
+            side ? "" : "content-screen"
+          }`}
+        >
+          {board.columns.map((col, _) => {
+            return <BoardStatus key={col.id} boardId={col.id} column={col} />;
+          })}
+          {/* TODO : the new column div is set to handle editBoard */}
+          <div className="new-column" onClick={() => setEditColumn(true)}>
+            <p>+ New Column</p>
+          </div>
         </div>
       </div>
-    </div>
+      <EditBoard isOpen={editColumn} onClose={() => setEditColumn(false)} />
+    </>
   );
 }
 
