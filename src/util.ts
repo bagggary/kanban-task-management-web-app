4a6f8e11ca09a28695fda321c7c472d50c482370
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-02-02 23:08:59 +0300

    update utils functions

--- src/util.ts
+++ src/util.ts
@@ -16,3 +16,21 @@ export const generateId = (letter: number, number: number) => {
   }
   return id;
 };
+
+export function getRandomColor(index: number) {
+  const colors = [
+    "rgb(73, 196, 229)",
+    "rgb(132, 113, 242)",
+    "rgb(103, 226, 174)",
+  ];
+  if (index < colors.length) {
+    return colors[index];
+  } else {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+}
