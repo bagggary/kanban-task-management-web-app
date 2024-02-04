8cfed34e95c8b2baaf7a9594a6c6a011d54bb2a8
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-02-04 03:41:40 +0300

    sortable tasks to implement drag and drop

--- src/UI/Tasks/SortableTasks.jsx
+++ src/UI/Tasks/SortableTasks.jsx
@@ -1,8 +1,8 @@
-import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import { useDataContext } from "../../context/DataContext";
 import { useIdContext } from "../../context/IdContext";
 import Tasks from "./Tasks";
-import { CSS } from "@dnd-kit/utilities";
+import { useSortable } from "@dnd-kit/sortable";
 
 export function SortableTasks({ taskId }) {
   const { data } = useDataContext();
@@ -17,12 +17,40 @@ export function SortableTasks({ taskId }) {
     (currentTask) => currentTask.id === taskId
   )[0];
 
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: taskId,
+    data: {
+      type: "Task",
+      task,
+    },
+  });
+
+  const style = {
+    transition,
+    transform: CSS.Translate.toString(transform),
+  };
+
+  if (isDragging) {
+    return <div className="dragging-task" ref={setNodeRef} style={style}></div>;
+  }
+
   return (
-    <Tasks
-      task={task}
-      columnId={column.id}
-      subtasks={task.subtasks}
-      id={taskId}
-    />
+    <>
+      <div style={style} ref={setNodeRef} {...listeners} {...attributes}>
+        <Tasks
+          task={task}
+          columnId={column.id}
+          subtasks={task.subtasks}
+          key={taskId}
+        />
+      </div>
+    </>
   );
 }
