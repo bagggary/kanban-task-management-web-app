d7abc0b8131e8114a4df9aab597226f4822cd36e
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-02-05 21:39:34 +0300

    update infinite loop when dragging tasks

--- src/UI/Home/home.jsx
+++ src/UI/Home/home.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useSideContext } from "../../context/SideToggle";
 import { useDataContext } from "../../context/DataContext";
 import { useIdContext } from "../../context/IdContext";
@@ -8,19 +8,11 @@ import EditBoard from "../Modal/BoardModal/EditBoard";
 import {
   DndContext,
   DragOverlay,
-  KeyboardSensor,
   PointerSensor,
-  closestCenter,
-  closestCorners,
-  rectIntersection,
   useSensor,
   useSensors,
 } from "@dnd-kit/core";
-import {
-  SortableContext,
-  arrayMove,
-  sortableKeyboardCoordinates,
-} from "@dnd-kit/sortable";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
 import { SortableTasks } from "../Tasks/SortableTasks";
 import { createPortal } from "react-dom";
 
@@ -30,41 +22,16 @@ function Home() {
   const { id } = useIdContext();
   const [editColumn, setEditColumn] = useState(false);
   const board = data.filter((boardData) => boardData.id === id)[0];
-  const [activeId, setActiveId] = useState(null);
+  const [activeTask, setActiveTask] = useState(null);
   const [activeColumn, setActiveColumn] = useState(null);
+  const [updateData, setUpdateData] = useState(data);
+
   const columnId = useMemo(
     () => board.columns.map((col) => col.id),
     [board.columns]
   );
 
-  const defaultAnnouncements = {
-    onDragStart(id) {
-      return `Picked up draggable item ${id}.`;
-    },
-    onDragOver(id, overId) {
-      if (overId) {
-        return `Draggable item ${id} was moved over droppable area ${overId}.`;
-      }
-
-      return `Draggable item ${id} is no longer over a droppable area.`;
-    },
-    onDragEnd(id, overId) {
-      if (overId) {
-        return `Draggable item was dropped over droppable area ${overId}`;
-      }
-
-      return `Draggable item ${id} was dropped.`;
-    },
-    onDragCancel(id) {
-      return `Dragging was cancelled. Draggable item ${id} was dropped.`;
-    },
-  };
-
   const sensors = useSensors(
-    // useSensor(PointerSensor),
-    // useSensor(KeyboardSensor, {
-    //   coordinateGetter: sortableKeyboardCoordinates,
-    // })
     useSensor(PointerSensor, {
       activationConstraint: {
         distance: 10,
@@ -73,18 +40,170 @@ function Home() {
   );
 
   const handleDragStart = (e) => {
-    // setActiveId(e.active.id);
-    console.log("Drag Start ", e);
     if (e.active.data.current.type === "Column") {
       setActiveColumn(e.active.data.current.column);
+    }
+
+    if (e.active.data.current.type === "Task") {
+      setActiveTask(e.active.data.current.task);
     }
   };
 
   const handleDragOver = (e) => {
-    console.log("handleDragOver", e);
+    const { active, over } = e;
+
+    setUpdateData((prev) => {
+      return moveTaskAndUpdateData(prev, active, over);
+    });
+  };
+
+  useEffect(() => {
+    if (updateData !== null) {
+      setData(updateData);
+    }
+  }, [updateData]);
+
+  const moveTaskAndUpdateData = (prevData, active, over) => {
+    if (!over) return prevData;
+
+    const activeId = active.id;
+    const overId = over.id;
+
+    if (activeId === overId) return prevData;
+
+    const isActiveATask = active.data.current.type === "Task";
+    const isOverATask = over.data.current.type === "Task";
+
+    if (!isActiveATask) return prevData;
+
+    let updatedData = [...prevData];
+
+    if (isActiveATask && isOverATask) {
+      // if the active task is not in the same column
+      if (active.data.current.task.status !== over.data.current.task.status) {
+        const sourceColumn = board.columns.find((col) =>
+          col.tasks.some((task) => task.id === activeId)
+        );
+
+        const destinationColumn = board.columns.find((col) =>
+          col.tasks.some((task) => task.id === overId)
+        );
+
+        if (sourceColumn && destinationColumn) {
+          const sourceColumnIndex = board.columns.indexOf(sourceColumn);
+          const destinationColumnIndex =
+            board.columns.indexOf(destinationColumn);
+
+          const sourceTaskIndex = sourceColumn.tasks.findIndex(
+            (task) => task.id === activeId
+          );
+
+          let updatedColumns = [...board.columns];
+          let removedTask = updatedColumns[sourceColumnIndex].tasks.splice(
+            sourceTaskIndex,
+            1
+          )[0];
+
+          const overTaskIndex = destinationColumn.tasks.findIndex(
+            (task) => task.id === overId
+          );
+
+          removedTask.status =
+            destinationColumn.tasks.length > 0
+              ? destinationColumn.tasks[overTaskIndex].status
+              : destinationColumn.name;
+
+          destinationColumn.tasks.splice(overTaskIndex, 0, removedTask);
+
+          const updatedBoard = { ...board, columns: updatedColumns };
+
+          const boardIndex = updatedData.findIndex(
+            (currentBoard) => currentBoard.id === id
+          );
+
+          updatedData[boardIndex] = updatedBoard;
+        }
+      } else {
+        // if task is in the same column
+        const currentColumn = board.columns.find((col) =>
+          col.tasks.some((task) => task.id === activeId)
+        );
+
+        const currentColumnIndex = board.columns.findIndex(
+          (col) => col.id === currentColumn.id
+        );
+
+        const currentBoardIndex = updatedData.findIndex(
+          (currentBoard) => currentBoard.id === id
+        );
+
+        const columns = [...board.columns];
+        const tasks = columns[currentColumnIndex].tasks;
+
+        const activeTaskIndex = tasks.findIndex(
+          (activeTask) => activeTask.id === activeId
+        );
+
+        const overTaskIndex = tasks.findIndex(
+          (overTask) => overTask.id === overId
+        );
+
+        // Remove the task from its original position
+        const removedTask = tasks.splice(activeTaskIndex, 1)[0];
+
+        // Add the task to the new position
+        tasks.splice(overTaskIndex, 0, removedTask);
+
+        updatedData[currentBoardIndex].columns[currentColumnIndex].tasks =
+          tasks;
+      }
+    }
+
+    const isOverAColumn = over.data.current.type === "Column";
+
+    // if task is dragged to an empty column
+    if (isActiveATask && isOverAColumn) {
+      const sourceColumn = board.columns.find((col) =>
+        col.tasks.some((task) => task.id === activeId)
+      );
+
+      const destinationColumn = board.columns.find((col) => col.id === overId);
+
+      if (sourceColumn && destinationColumn) {
+        const sourceColumnIndex = board.columns.indexOf(sourceColumn);
+        const destinationColumnIndex = board.columns.indexOf(destinationColumn);
+
+        const sourceTaskIndex = sourceColumn.tasks.findIndex(
+          (task) => task.id === activeId
+        );
+
+        let updatedColumns = [...board.columns];
+
+        let removedTask = updatedColumns[sourceColumnIndex].tasks.splice(
+          sourceTaskIndex,
+          1
+        )[0];
+
+        removedTask.status = board.columns[destinationColumnIndex].name;
+
+        destinationColumn.tasks.push(removedTask);
+
+        const updatedBoard = { ...board, columns: updatedColumns };
+
+        const boardIndex = updatedData.findIndex(
+          (currentBoard) => currentBoard.id === id
+        );
+
+        updatedData[boardIndex] = updatedBoard;
+      }
+    }
+
+    return updatedData;
   };
 
   const handleDragEnd = (e) => {
+    setActiveColumn(null);
+    setActiveTask(null);
     const { active, over } = e;
     if (!over) return;
     const activeColumnId = active.id;
@@ -114,11 +233,9 @@ function Home() {
       <div className="content">
         <Sidenav handleBoard={(e) => handleActive(e.target.id)} />
         <DndContext
-          // announcements={defaultAnnouncements}
           sensors={sensors}
-          // collisionDetection={closestCorners}
           onDragStart={handleDragStart}
-          // onDragOver={handleDragOver}
+          onDragOver={handleDragOver}
           onDragEnd={handleDragEnd}
         >
           <div
@@ -140,6 +257,7 @@ function Home() {
               {activeColumn && (
                 <BoardStatus column={activeColumn} boardId={activeColumn.id} />
               )}
+              {activeTask && <SortableTasks taskId={activeTask.id} />}
             </DragOverlay>,
             document.body
           )}
@@ -151,26 +269,3 @@ function Home() {
 }
 
 export default Home;
-
-{
-  /* <DragOverlay>
-            {activeId ? <SortableTasks taskId={activeId} /> : null}
-          </DragOverlay> */
-}
-{
-  /* <DragOverlay>
-            {activeId ? (
-              <div
-                style={{
-                  pointerEvents: "none",
-                  position: "fixed",
-                  zIndex: 100,
-                  top: 0,
-                  left: 0,
-                }}
-              >
-                <SortableTasks taskId={activeId} />
-              </div>
-            ) : null}
-          </DragOverlay> */
-}
