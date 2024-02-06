aebf1f0259134fb2aceecca4c995bdd8d1ccc359
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-02-07 01:34:09 +0300

    updating board delete functions

--- src/UI/Modal/BoardModal/BoardDelete.jsx
+++ src/UI/Modal/BoardModal/BoardDelete.jsx
@@ -4,10 +4,11 @@ import { useIdContext } from "../../../context/IdContext";
 
 export default function BoardDelete({ isOpen, onClose }) {
   const { data, setData } = useDataContext();
-  const { id } = useIdContext();
+  const { id, setId } = useIdContext();
 
-  const currentBoardIndex =
-    data && data.findIndex((currentBoard) => currentBoard.id === id);
+  const currentBoardIndex = id
+    ? data && data.findIndex((currentBoard) => currentBoard.id === id)
+    : 0;
   useEffect(() => {
     const clickOutside = (e) => {
       if (e.target.className === "overlay show") {
@@ -21,19 +22,16 @@ export default function BoardDelete({ isOpen, onClose }) {
   }, []);
 
   function deleteBoard() {
-    if (data.length - 1 <= 0) {
-      return;
-    }
     let updatedData = [...data];
     updatedData.splice(currentBoardIndex, 1);
 
     // TODO : Fixing the existing board selection disappear when deleteing the selected board
-    // TODO : Figure out how to prevent errors when there's not selecting id ;
+    // TODO : Figure out how to prevent errors when there's not selecting id ; // DONE
 
     setData(updatedData);
-    if (currentBoardIndex == data.length - 1) {
-      return;
-    }
+    setId(null);
+
+    console.log(data);
     onClose();
   }
 
@@ -43,8 +41,8 @@ export default function BoardDelete({ isOpen, onClose }) {
         <h1>Delete this board?</h1>
         <p>
           Are you sure you want to delete the '
-          <span>{data[currentBoardIndex].name}</span>' board? This action will
-          remove all columns and tasks and cannot be reversed.
+          <span>{data && data[currentBoardIndex]?.name}</span>' board? This
+          action will remove all columns and tasks and cannot be reversed.
         </p>
         <div className="delete-options">
           <button onClick={deleteBoard}>Delete</button>
